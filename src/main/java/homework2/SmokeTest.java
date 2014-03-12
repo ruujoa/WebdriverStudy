aba10da02a8d81c39715181676198781a06395c5
ruujoa <yao3000vlove1999@hotmail.com>
2014-03-12 16:41:37 +0800

    add log4j support

--- src/main/java/homework2/SmokeTest.java
+++ src/main/java/homework2/SmokeTest.java
@@ -3,9 +3,13 @@ package homework2;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
@@ -14,10 +18,13 @@ public class SmokeTest {
 	private List<BrowsersType> excludes = null;
 	private Action action = new Action();
 	private long now = 0;
+	private final Logger logger = Logger.getLogger( getClass() );
 	
-	@BeforeClass
-	@Parameters({"url", "browsersToExclude"})
-	public void start(String url, String excludes) {
+	@BeforeTest
+	@Parameters({"browsersToExclude"})
+	public void prepare(String excludes) {
+		PropertyConfigurator.configure( System.getProperty( "user.dir" ) + "/tool/log4j.properties" );
+		logger.info( "The test starts..." );
 		now = System.currentTimeMillis();
 	    this.excludes = new ArrayList<BrowsersType>();
 	    if ( excludes.indexOf(',') > -1 ) {
@@ -28,7 +35,12 @@ public class SmokeTest {
 	    } else {
 	    	this.excludes.add(BrowsersType.valueOf(excludes));
 	    }
-	    action.init(url, this.excludes);
+	}
+	
+	@BeforeClass
+	@Parameters({"url"})
+	public void start(String url) {
+		action.init(url, this.excludes);
 	}
 	
 	@Test
@@ -48,8 +60,14 @@ public class SmokeTest {
 	public void end() {
 		action.logout();
 		action.close();
-		System.out.println( "This test costs " 
+		
+	}
+	
+	@AfterTest
+	public void log() {
+		logger.info( "The test ends..." );
+		logger.info( "The test costs " 
 			+ (System.currentTimeMillis() - now)/1000
-			+ " seconds." );
+			+ " seconds.\n" );
 	}
 }
